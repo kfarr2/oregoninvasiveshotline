0868f587e0c82062f75d13b7ee50826d955c01a0
Konstantin <kfarr2@pdx.edu>
2015-09-25 14:43:38 -0700

    Update Makefile to run arctasks

---
+++ setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+
+install_requires = [
+    'django',
+    'git+https://github.com/PSU-OIT-ARC/django-arcutils.git#egg=django-arcutils',
+    '-e git+https://github.com/PSU-OIT-ARC/arctasks#egg=psu.oit.arc.tasks',
+    'django-cloak',
+    'git+https://github.com/PSU-OIT-ARC/django-local-settings.git#egg=django_local_settings'
+    'django-perms',
+    'git+https://github.com/PSU-OIT-ARC/elasticmodels.git#egg=elasticmodels',
+    'elasticsearch',
+    'elasticsearch-dsl',
+    'Markdown',
+    'mccabe',
+    'mock',
+    'mommy-spatial-generators',
+    'pbr',
+    'pep8',
+    'Pillow',
+    'psycopg2',
+    'pyflakes',
+    'python-dateutil',
+    'pytz',
+    'six',
+    'stashward',
+    'urllib3',
+    'wheel',
+]
+
+
+setup(
+    name='psu.oit.arc.oregoninvasiveshotline',
+    version=VERSION,
+    description='Oregon Invasives Hotline',
+    author='PSU - OIT - ARC',
+    author_email='consultants@pdx.edu',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'dev': [
+            'bpython',
+            'coverage',
+            'flake8',
+            'isort',
+            'model_mommy',
+            'psu.oit.arc.tasks',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
---
+++ tasks.py
@@ -0,0 +1,16 @@
+import os
+import platform
+
+from arctasks import *
+from arctasks.django import manage
+
+@arctask(configured="dev", timed=True)
+def init(ctx, overwrite=False):
+    virtualenv(ctx, overwrite=overwrite)
+    install(ctx)
+    createdb(ctx, drop=overwrite)
+    manage(ctx, 'migrate')
+    manage(ctx, 'loaddata dummy_user.json category.json severity.json species.json counties.json pages.json')
+    manage(ctx, 'rebuild_index --clopen --noinput')
+    manage(ctx, 'collectstatic --noinput')
+    manage(ctx, 'test')
